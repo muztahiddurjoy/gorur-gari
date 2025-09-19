068bf1dd13b0cc66e91d0cffe478bf7376bb1770
muztahiddurjoy <muztahiddurjoy99@gmail.com>
2025-09-19 22:08:18 +0600

    Add bounds checking for servo angle and update servo control in main loop

--- firmware/platform_io/lib/servo_controller/servo_controller.cpp
+++ firmware/platform_io/lib/servo_controller/servo_controller.cpp
@@ -3,12 +3,14 @@
 ServoController::ServoController(int pin) : _pin(pin), _currentAngle(0) {}
 
 void ServoController::begin() {
+    // Remove analogWrite line - not needed for servo
     _servo.attach(_pin);
     _servo.write(_currentAngle);
 }
 
 void ServoController::setAngle(int angle) {
-    _currentAngle = angle;
+    // Add bounds checking for servo angle (typically 0-180 degrees)
+    _currentAngle = constrain(angle, 0, 180);
     _servo.write(_currentAngle);
 }
 
